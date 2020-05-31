149c076cbd3a7a56dacf4158d7f40f46a1dfe5f3
rendo <rendo@DESKTOP-0FGPCGK>
2020-05-31 18:57:14 -0500

    5

--- ProyectoFinalProcesos/src/controlador/VistaInformacionDelCursoController.java
+++ ProyectoFinalProcesos/src/controlador/VistaInformacionDelCursoController.java
@@ -6,7 +6,9 @@
 package controlador;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -14,6 +16,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+import modelo.CursoVO;
 
 /**
  * FXML Controller class
@@ -52,17 +56,68 @@ public class VistaInformacionDelCursoController implements Initializable {
     private TableColumn<?, ?> columnaClientes;
     @FXML
     private Button botonAceptar;
+    
+    private CursoVO curso;
+    private ObservableList<CursoVO> cursosEnTabla;
+    @FXML
+    private Label labelCategoria;
+    @FXML
+    private Label setCategoriaLabel;
+    
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        //llenarDatos();
     }    
 
     @FXML
     private void aceptar(ActionEvent event) {
+        Stage stage = (Stage) this.botonAceptar.getScene().getWindow();
+        stage.close();
     }
+    
+    public void initDatos(ObservableList<CursoVO> cursos, CursoVO curso ){
+        this.cursosEnTabla = cursos;
+        this.curso = curso;
+        llenarDatos();
+    }
+    
+    public void llenarDatos(){
+        this.setCreditosLabel.setText(curso.getCreditos()+"");
+        this.setNrcLabel.setText(curso.getNrc());
+        this.setCupoLabel.setText(curso.getCupo()+"");
+        this.setFechaFinLanel.setText(formatoFecha (curso.getFechaFin().toString()));
+        this.setFechaInicioLabel.setText(formatoFecha(curso.getFechaInicio().toString()));
+        this.setNombreLabel.setText(curso.getNombre());
+        this.setCategoriaLabel.setText(curso.getCategoria());
+    }
+    
+    public String formatoFecha(String fecha){
+        String fechaFormato;
+        char [] fechaArray = fecha.toCharArray();
+        StringBuilder anio = new StringBuilder();
+        anio.append(fechaArray[0]);
+        anio.append(fechaArray[1]);
+        anio.append(fechaArray[2]);
+        anio.append(fechaArray[3]);
+        
+        StringBuilder mes = new StringBuilder();
+        mes.append(fechaArray[5]);
+        mes.append(fechaArray[6]);
+                
+        StringBuilder dia = new StringBuilder();
+        dia.append(fechaArray[8]);
+        dia.append(fechaArray[9]);
+        
+        fechaFormato = dia.toString() + "/" + mes.toString()+"/" + anio.toString();
+        
+        
+        
+        return fechaFormato;
+    }
+     
     
 }
